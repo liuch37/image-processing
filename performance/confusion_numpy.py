8a5633b22cc2d32c137aff1a6bb2162afc5ed63d
Chun-Hao Liu <chun-haoliu@chun-haos-air.lan>
2021-05-29 22:43:36 -0700

    Add confusion matrix plotting code

---
+++ performance/confusion_numpy.py
@@ -0,0 +1,99 @@
+'''
+This is to implement confusion matrix computation and its plotting function.
+This code is modified from https://www.kaggle.com/grfiv4/plot-a-confusion-matrix.
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+def plot_confusion_matrix(cm,
+                          target_names=None,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+    """
+    given a sklearn confusion matrix (cm), make a nice plot
+
+    Arguments
+    ---------
+    cm:           confusion matrix from sklearn.metrics.confusion_matrix
+
+    target_names: given classification classes such as [0, 1, 2]
+                  the class names, for example: ['high', 'medium', 'low']
+
+    title:        the text to display at the top of the matrix
+
+    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                  see http://matplotlib.org/examples/color/colormaps_reference.html
+                  plt.get_cmap('jet') or plt.cm.Blues
+
+    Usage
+    -----
+    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
+                                                              # sklearn.metrics.confusion_matrix
+                          normalize    = True,                # show proportions
+                          target_names = y_labels_vals,       # list of names of the classes
+                          title        = best_estimator_name) # title of graph
+
+    Citiation
+    ---------
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    """
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names)
+        plt.yticks(tick_marks, target_names)
+    else:
+        tick_marks = np.arange(len(cm))
+        plt.xticks(tick_marks, tick_marks)
+        plt.yticks(tick_marks, tick_marks)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    plt.ylabel('True Label')
+    plt.xlabel('Predicted Label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.savefig('confusion_matrix.png')
+    plt.show()
+
+# unit testing
+if __name__ == '__main__':
+    # example 1:
+    plot_confusion_matrix(cm=np.array([[ 1098,  1934,   807],
+                                       [  604,  4392,  6233],
+                                       [  162,  2362, 31760]]),
+                          cmap=plt.get_cmap('Reds'),
+                          normalize=False,
+                          target_names=['high', 'medium', 'low'],
+                          title="Confusion Matrix")
+    # example 2:
+    plot_confusion_matrix(cm=np.array([[ 1098,  1934,   807],
+                                       [  604,  4392,  6233],
+                                       [  162,  2362, 31760]]), 
+                          normalize=True,
+                          target_names=['high', 'medium', 'low'],
+                          title="Normalized Confusion Matrix")
